e45c14e9035fc20c7a61a092ea53783fe5d5faaf
Kucherenko Sergey <kucherenko.web@gmail.com>
2019-05-04 02:36:34 +0300

    Updates

---
+++ precache-manifest.9bec49af1b716e2ea650e3c37a1b7bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ad03eb845f62b49a98e49e86bc45dc",
+    "url": "/cherenkor/react-blog/index.html"
+  },
+  {
+    "revision": "6b6d131e3d3971e58885",
+    "url": "/cherenkor/react-blog/static/css/main.c54c4620.chunk.css"
+  },
+  {
+    "revision": "fe7b3789ecc72f3c3849",
+    "url": "/cherenkor/react-blog/static/js/2.77db8355.chunk.js"
+  },
+  {
+    "revision": "6b6d131e3d3971e58885",
+    "url": "/cherenkor/react-blog/static/js/main.47d09464.chunk.js"
+  },
+  {
+    "revision": "8761cc37c717549ff8f0",
+    "url": "/cherenkor/react-blog/static/js/runtime~main.0af7d50e.js"
+  }
+]);
