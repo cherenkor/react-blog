536bce6388be16be11b4318bfca3ffbb9ba58c1b
Kucherenko Sergey <kucherenko.web@gmail.com>
2019-05-04 02:30:13 +0300

    Change limit

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -2,7 +2,7 @@ import { FETCH_POSTS, NEW_POST, DELETE_POST } from './types';
 import axios from 'axios';
 import config from '../config/config';
 
-export const fetchPosts = (limit = 10) => dispatch => {
+export const fetchPosts = (limit = 3) => dispatch => {
     const limitQuery = `?_limit=${limit}`;
     axios
         .get(`${config.baseUrl}/posts${limitQuery}`)
